f9388080d83279b319b656562a52dc73e77490c9
NerdyJ23 <jessica.moolenschot@outlook.com>
2022-12-23 22:51:23 +1100

    fix mdi icons not displaying

--- site/src/main.js
+++ site/src/main.js
@@ -8,6 +8,9 @@ import NotFoundPage from './pages/ErrorPages/NotFoundPage';
 import vuetify from './plugins/vuetify'
 import store from './store'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 // import '@babel/polyfill'
 
--- site/src/plugins/vuetify.js
+++ site/src/plugins/vuetify.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-
-import colors from 'vuetify/lib/util/colors'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
-export default new Vuetify();
+export default new Vuetify({
+});
