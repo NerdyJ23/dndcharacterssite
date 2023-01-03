d5d67f2ed6d9d1dc9ae0ee5ecaa1320ccc6efe28
NerdyJ23 <surefiremooly@gmail.com>
2023-01-03 09:51:40 +0000

    Create list method for characterstats endpoint

--- api/src/Controller/CharactersStatsController.php
+++ api/src/Controller/CharactersStatsController.php
@@ -2,10 +2,7 @@
 namespace App\Controller;
 
 use Cake\Controller\Controller;
-use Cake\View\JsonView;
-use App\Controller\Security\EncryptionController;
-use App\Controller\Security\AuthenticationController;
-use Cake\Http\Cookie\Cookie;
+use App\Controller\Component\Enum\StatusCodes;
 
 class CharactersStatsController extends ApiController {
 	public function initialize(): void {
@@ -13,6 +10,30 @@ class CharactersStatsController extends ApiController {
 	}
 
 	public function list() {
+		$limit = $this->request->getQuery('limit') == null ? 200 : $this->request->getQuery('limit');
+		$page = $this->request->getQuery('page') == null ? 1 : $this->request->getQuery('page');
+		$count = $this->request->getQuery('count') == null ? false : true;
 
+		$token = $this->request->getCookie('token');
+		$id = $this->request->getParam("character_id");
+
+		$charDB = new CharactersController();
+		$char = $charDB->getById($this->decrypt($id), $token);
+		if (sizeOf($char) == 0) {
+			return $this->response(StatusCodes::NOT_FOUND);
+		}
+		$query = $this->CharactersStats->find('all')
+		->where(['Char_ID' => $char[0]->ID]);
+
+		if ($query == null) {
+			return $this->response(StatusCodes::NOT_FOUND);
+		}
+
+		$result = $query->all()->toArray();
+		$this->set("result", sizeOf($result) == 0 ? [] : $result);
+		$this->set("page", $page);
+		$this->set("limit", $limit);
+		$this->set("count", sizeOf($result));
+		return;
 	}
 }
