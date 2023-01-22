cf5ff5ad4866e1f30f039ef1215b71c2300e0d29
NerdyJ23 <surefiremooly@gmail.com>
2023-01-22 08:51:36 +0000

    Move characters background to Client

--- api/src/Client/Characters/CharactersBackgroundClient.php
+++ api/src/Client/Characters/CharactersBackgroundClient.php
@@ -3,15 +3,28 @@
 namespace App\Client\Characters;
 
 use App\Client\AbstractClient;
+use App\Client\Characters\CharactersClient;
 
 class CharactersBackgroundClient extends AbstractClient{
-	public function create(int $charId, object $background): string {
-		$backgroundItem = parent::fetchTable('CharactersBackgrounds')->newEntity([
+	const TABLE = "CharactersBackgrounds";
+
+	static function list(int $id, string $token) {
+		$char = CharactersClient::get($id, $token);
+		if ($char != null) {
+			if (property_exists($char, "Background")) {
+				return $char->background;
+			}
+		}
+		return null;
+	}
+
+	static function create(int $charId, object $background): string {
+		$backgroundItem = parent::fetchTable(CharactersBackgroundClient::TABLE)->newEntity([
 			'Char_ID' => $charId,
 			'Name' => $background->name,
 			'Description' => $background->description
 		]);
-		$result = parent::fetchTable('CharactersBackgrounds')->save($backgroundItem);
+		$result = parent::fetchTable(CharactersBackgroundClient::TABLE)->save($backgroundItem);
 
 		if ($result != null) {
 			return $result->id;
--- api/src/Controller/CharactersBackgroundController.php
+++ api/src/Controller/CharactersBackgroundController.php
@@ -3,7 +3,10 @@ namespace App\Controller;
 use Cake\Controller\Controller;
 use App\Controller\Component\Enum\StatusCodes;
 use App\Controller\Component\Pagination;
-use App\Schema\Character\CharacterBackgroundSchema;
+use App\Schema\AbstractSchema;
+
+use App\Client\Security\AuthClient;
+use App\Client\Characters\CharactersBackgroundClient;
 
 class CharactersBackgroundController extends ApiController {
 	public function initialize(): void {
@@ -13,43 +16,17 @@ class CharactersBackgroundController extends ApiController {
 	//Character should only have one background
 	public function list() {
 		$token = $this->request->getCookie('token');
-		$validToken = $token == null ? false : (new AuthenticationController)->validToken($token);
+		// $validToken = $token == null ? false : AuthClient::validToken($token);
 
 		$id = $this->request->getParam("character_id");
-		$charId = $this->decrypt($id);
-		if ($charId == false) {
+		$background = CharactersBackgroundClient::list($this->decrypt($id), $token);
+		$this->set("bg", $background);
+		return;
+		if ($background == null) {
 			return $this->response(StatusCodes::NOT_FOUND);
 		}
-
-		$charDB = new CharactersController();
-		$char = $charDB->getById($this->decrypt($id), $token);
-		if (sizeOf($char) == 0) {
-			return $this->response(StatusCodes::NOT_FOUND);
-		}
-
-		if ($char[0]->Visibility == 1) {
-			$query = $this->fetchTable('CharactersBackgrounds')->find('all')
-			->where(['Char_ID' => $char[0]->ID]);
-		} else {
-			$userDB = new UsersController();
-			$user = $userDB->getByToken($token);
-
-			if ($user == null) {
-				return $this->response(StatusCodes::TOKEN_MISMATCH);
-			}
-			if ($user->ID == $char[0]->User_Access) {
-				$this->fetchTable('CharactersBackgrounds')->find('all')
-				->where(['Char_ID' => $char[0]->ID]);
-			} else {
-				return $this->response(StatusCodes::NOT_FOUND);
-			}
-		}
-		$data = $query->all();
-		$resultSet = [];
-
-		foreach($data as $item) {
-			$resultSet[] = CharacterBackgroundSchema::toSummarizedSchema($item);
-		}
-		$this->set("result", $resultSet);
+		$this->set("result", AbstractSchema::schema($background, "CharacterBackground"));
+		$this->response = $this->response(StatusCodes::SUCCESS);
+		return;
 	}
 }
