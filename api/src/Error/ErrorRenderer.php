ff586a3de2bbf5aeec4bc78267af5ea8733174ec
NerdyJ23 <surefiremooly@gmail.com>
2023-01-26 06:53:20 +0000

    Add usernotfoundexceptoin

--- api/src/Error/ErrorRenderer.php
+++ api/src/Error/ErrorRenderer.php
@@ -2,19 +2,33 @@
 namespace App\Error;
 
 use Cake\Error\ExceptionRenderer;
-use Cake\View\JsonView;
 
 class ErrorRenderer extends ExceptionRenderer {
 
-    public function initialize(): void {
-        $this->viewBuilder()->setClassName('Json');
+    //Custom Errors
+    public function logic($e) {
+        return $this->sendResponse($e->getCode(), $e->getMessage());
     }
 
-    public function logic($e) {
-        $res = $this->controller->getResponse();
-        $res = $res->withType('application/json');
-        $res = $res->withStringBody(json_encode(['errorMessage ' => $e->getMessage()]));
-        $res = $res->withStatus($e->getCode());
-        return $res;
+    public function userNotFound($e) {
+        return $this->sendResponse($e->getCode(), $e->getMessage());
+    }
+
+    private function sendResponse(int $code, string $message) {
+        try {
+            $res = $this->controller->getResponse();
+            $res = $res->withType('application/json');
+            $res = $res->withStringBody(json_encode(['errorMessage ' => $message]));
+            $res = $res->withStatus($code);
+            return $res;
+        } catch(Exception $e) {
+            return $this->controller->getResponse()->withStringBody("ERROR")->withStatus(500);
+        }
+    }
+
+    //Default Errors
+
+    public function internalError($e) {
+        return $this->sendResponse($e->getCode(), $e->getMessage());
     }
 }
