016df62ea61b69aff7feabd0b8b7505896418ba2
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-02-06 20:54:53 +1100

    Create pagination component

---
+++ site/src/components/Utility/Pagination.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-container>
+		<v-btn v-for="a in pageCount" @click="$emit('pageChanged', a)">{{ a }}</v-btn>
+	</v-container>
+</template>
+<script>
+export default {
+	name: "Pagination",
+	props: {
+		page: {
+			type: Number,
+			required: false,
+			default: 1
+		},
+		limit: {
+			type: Number,
+			required: false,
+			default: 30
+		},
+		count: {
+			type: Number,
+			required: false,
+			default: 0
+		}
+	},
+	computed: {
+		pageCount() {
+			return (this.limit * this.page / this.count);
+		}
+	}
+}
+</script>
